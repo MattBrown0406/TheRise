b899f8298e856c39a1240a89809a2e5f75f74877
Matt Brown <mattbrown@Matts-MacBook-Air-5.local>
2026-07-04 11:28:19 -0700

    Add Xcode iOS app build

---
+++ ios/TheRise/TheRise/RiseViewController.swift
@@ -0,0 +1,40 @@
+import UIKit
+import WebKit
+
+final class RiseViewController: UIViewController, WKNavigationDelegate {
+    private lazy var webView: WKWebView = {
+        let configuration = WKWebViewConfiguration()
+        configuration.defaultWebpagePreferences.allowsContentJavaScript = true
+
+        let webView = WKWebView(frame: .zero, configuration: configuration)
+        webView.navigationDelegate = self
+        webView.allowsBackForwardNavigationGestures = false
+        webView.isOpaque = false
+        webView.backgroundColor = UIColor(red: 0.969, green: 0.957, blue: 0.925, alpha: 1)
+        webView.scrollView.backgroundColor = webView.backgroundColor
+        return webView
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = webView.backgroundColor
+        view.addSubview(webView)
+        webView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            webView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            webView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            webView.topAnchor.constraint(equalTo: view.topAnchor),
+            webView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+        loadRiseApp()
+    }
+
+    private func loadRiseApp() {
+        guard let url = Bundle.main.url(forResource: "the-rise-app", withExtension: "html", subdirectory: "Web") else {
+            assertionFailure("Missing bundled The Rise HTML app.")
+            return
+        }
+        let accessURL = url.deletingLastPathComponent()
+        webView.loadFileURL(url, allowingReadAccessTo: accessURL)
+    }
+}
